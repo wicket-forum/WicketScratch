13d08e36ae90b20d9189f66ad8df5a642e3a90e8
mroedig <mroedig@gmail.com>
2013-11-04 22:42:37 +0100

    Wicket Scratch Teil 2 - Kommunikation

--- WicketScratch/src/main/java/de/wicketforum/app/HomePage.java
+++ WicketScratch/src/main/java/de/wicketforum/app/HomePage.java
@@ -2,19 +2,43 @@ package de.wicketforum.app;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
 
 public class HomePage extends WebPage {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1337853758160866119L;
 	
+	private static final long serialVersionUID = 3436333845493815637L;
+	private String feldInhalt;
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
-		
+		PropertyModel<String> feldModel = new PropertyModel<String>(this, "feldInhalt");
 		add(new Label("helloWicket", "HelloWicketWorld"));
+		Form formular = new Form("formular") {
+		
+			private static final long serialVersionUID = 8919254699133944081L;
+
+			@Override
+			protected void onSubmit() {
+
+				this.setResponsePage(new AusgabePage(feldInhalt));
+
+			}
+		};
+		TextField<String> feld = new TextField<>("feld", feldModel);
+
+		formular.add(feld);
+
+		add(formular);
+
+	}
+	public String getFeldInhalt() {
+		return feldInhalt;
+	}
+	public void setFeldInhalt(String feldInhalt) {
+		this.feldInhalt = feldInhalt;
 	}
 
 }
